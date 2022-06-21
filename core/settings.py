0776cedbae9889716f9f26260aba21748a192c51
Vladimir <vladimirdev635@gmail.com>
2022-06-21 17:57:24 +0300

    add_phonenumbers_library

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'phonenumber_field',
+    
     'lei.apps.LeiConfig',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
